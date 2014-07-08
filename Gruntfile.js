0fdec9fd7eb49f20ec617fe6b136ba498728db10
SHTrassEr <shtrasser@gmail.com>
2014-07-08 11:22:16 +0900

    remove sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-ts');
 
   grunt.initConfig({
